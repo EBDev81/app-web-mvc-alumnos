361538646142f47bfedfcd950a120cd94f0867ec
Elías Barrera <eliasbarreramail@gmail.com>
2024-07-02 14:16:36 +0200

    Correcciones en usuarioRepository quitando imports no usados

--- app-web-mvc-alumnos/src/main/java/io/keepcoding/app/web/alumnos/repository/UsuarioRepository.java
+++ app-web-mvc-alumnos/src/main/java/io/keepcoding/app/web/alumnos/repository/UsuarioRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import io.keepcoding.app.web.alumnos.entity.Alumno;
 import io.keepcoding.app.web.alumnos.entity.Usuario;
 
 @Repository
